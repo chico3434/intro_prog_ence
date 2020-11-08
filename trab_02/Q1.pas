1d30f44f76b758f2304e9c8e39666448318991c3
chico3434 <chicokarkow@hotmail.com>
2020-11-07 23:35:28 -0300

    Questão 1 feita

---
+++ trab_02/Q1.pas
@@ -0,0 +1,19 @@
+{Faça um programa que calcule e imprima a soma dos 50 primeiros termos da série S,
+cujos 4 primeiros termos são dados abaixo:
+S = (1/2) - (3/4) + (5/8) - (7/16) + ...}
+Program Q1;
+var i,j,numerador:integer;
+	soma,denominador:real;
+Begin
+	soma:= 1/2;
+	numerador := 1;
+	for i:=2 to 50 do
+	begin
+		denominador:=1;
+		for j:=1 to i do denominador:=denominador*2;
+		numerador := numerador + 2;
+		if (i MOD 2 = 0) then soma := soma - (numerador/denominador)
+		else soma := soma + (numerador/denominador);
+	end;
+	writeln('A soma da série é ', soma);	
+End.
