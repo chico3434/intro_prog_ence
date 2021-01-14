da98d866344b42fe3a72c7e6778bc2b9ee91445c
chico3434 <chicokarkow@hotmail.com>
2021-01-14 00:24:40 -0300

    Questão 7 do Trabalho 3 feita

---
+++ trab_03/Q7.pas
@@ -0,0 +1,27 @@
+Program Q7;
+const TAM = 1000;
+type vetor = array[1..TAM] of char;
+
+function f_pos_ultima(v: vetor; N: integer): integer;
+var i: integer;
+begin
+		f_pos_ultima := 1;
+		for i := 1 to N do
+		if (v[i] = ' ') then f_pos_ultima := i+1; // pelos exemplos vou considerar que nenhuma frase termina com espaço
+end;
+
+var frase: vetor;
+	i: integer;
+	c: char;
+Begin
+	writeln('Digite uma ou mais palavras (entre * para encerrar a digitação)');
+	writeln('Tamanho máximo: ', TAM);
+	for i := 1 to TAM do
+	begin
+		write('Letra ', i, ': ');
+		readln(c);
+		if(c <> '*') then frase[i] := c
+		else break;    
+	end;
+	writeln('A última palavra começa no índice: ', f_pos_ultima(frase, i-1));
+End.
